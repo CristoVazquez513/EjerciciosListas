caabf8f42c41265f2bc8bb92605c83596274b062
Cristo Vazquez <c.j_13_10@hotmail.com>
2024-08-03 12:22:02 -0700

    Add project files.

---
+++ EjerciciosListas/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EjerciciosListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> diasSemana = new List<string>();
+            diasSemana.Add("1.- Lunes");
+            diasSemana.Add("2.- Martes");
+            diasSemana.Add("3.- Miercoles");
+            diasSemana.Add("4.- Jueves");
+            diasSemana.Add("5.- Viernes");
+            diasSemana.Add("6.- Sabado");
+            diasSemana.Add("7.- Domingo");
+
+            foreach (string dia in diasSemana) 
+            {
+                Console.WriteLine(dia);
+            }
+
+            //Console.WriteLine("Dame un numero de dia");
+            //string diaUsuario = Console.ReadLine();
+            //Console.WriteLine(diasSemana[int.Parse(diaUsuario)-1]);
+
+            Console.WriteLine(((int)DateTime.Now.DayOfWeek));
+            Console.WriteLine(diasSemana[((int)DateTime.Now.DayOfWeek)-1]);
+        }
+    }
+}
